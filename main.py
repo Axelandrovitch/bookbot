d68228da7abd66de10733b10d6ec17a3857aa56c
Axelandrovitch <axel.a.hetru@gmail.com>
2024-04-14 13:39:25 +0200

    add counter function and report function

---
+++ main.py
@@ -0,0 +1,26 @@
+def main():
+  with open("books/frankenstein.txt") as f:
+    file_contents = f.read()
+    words = file_contents.split()
+    
+    def char_counter():
+      char_appearances = {}
+      for char in file_contents.lower():
+        if char in char_appearances:
+          char_appearances[char] +=1
+        elif char != " " and char != "\n":
+          char_appearances[char] = 1
+      return char_appearances
+    
+    def report():
+      char_dictionnary = char_counter()
+      words_count = len(words)
+      print("--- Begin report of books/frankenstein.txt ---")
+      print(f"{words_count} words found in the document")
+      for k in char_dictionnary:
+        print(f"The {k} character was found {char_dictionnary[k]} times")
+      print("---End report ---")
+
+    print(char_counter())
+    report()
+main()
