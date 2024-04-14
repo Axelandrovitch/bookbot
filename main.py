2555c333cbdab51aa6e47875d8f4b9f4b5f7bd41
Axelandrovitch <axel.a.hetru@gmail.com>
2024-04-14 14:47:48 +0200

    add char_list to sort characters. Add isalpha method to filter out non-alpha characters. in report function loop over list instead of dictionnary

--- main.py
+++ main.py
@@ -4,21 +4,36 @@ def main():
     words = file_contents.split()
     
     def char_counter():
-      char_appearances = {}
+      char_appearances_dict = {}
+      char_appearances_list = []
       for char in file_contents.lower():
-        if char in char_appearances:
-          char_appearances[char] +=1
-        elif char != " " and char != "\n":
-          char_appearances[char] = 1
-      return char_appearances
+        if char.isalpha():
+          if char in char_appearances_dict:
+            char_appearances_dict[char] +=1
+          elif char != " " and char != "\n":
+            char_appearances_dict[char] = 1
+
+      for k in char_appearances_dict:
+        single_dict = {"char_name" : k, "appearances" : char_appearances_dict[k] }
+        char_appearances_list.append(single_dict)
+
+      
+      char_appearances_list.sort(reverse=True, key= sort_on_dict)
+
+      return char_appearances_dict, char_appearances_list
     
+
+    def sort_on_dict(dict):
+      return dict["appearances"]
+    
+
     def report():
-      char_dictionnary = char_counter()
+      char_dictionnary, char_list = char_counter()
       words_count = len(words)
       print("--- Begin report of books/frankenstein.txt ---")
       print(f"{words_count} words found in the document")
-      for k in char_dictionnary:
-        print(f"The {k} character was found {char_dictionnary[k]} times")
+      for char in char_list:
+        print(f"The {char['char_name']} character was found {char['appearances']} times")
       print("---End report ---")
 
     report()
